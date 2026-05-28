1f9464a8ac634ca18579a17c21d84f094d10b06d
Keerthana Malleshwaram <mallis.keerthana24@gmail.com>
2026-05-29 01:25:05 +0530

    Final database SSL fix

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-
-  baseURL:
-    import.meta.env.VITE_API_URL,
-
+  baseURL: "https://blog-platform-a23n.onrender.com/api",
 });
 
 export default API;
