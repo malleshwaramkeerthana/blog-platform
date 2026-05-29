96e0ff810b01a309b0a11e4803f7b9ae6f8886cd
Keerthana Malleshwaram <mallis.keerthana24@gmail.com>
2026-05-29 10:38:08 +0530

    Updated frontend UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,11 @@
 import { Link, useNavigate } from "react-router-dom";
-
 import "../styles/Navbar.css";
 
-function Navbar({ darkMode, setDarkMode ,}) {
+function Navbar() {
 
   const navigate = useNavigate();
 
   const token = localStorage.getItem("token");
-
   const username = localStorage.getItem("username");
 
   const handleLogout = () => {
@@ -15,96 +13,81 @@ function Navbar({ darkMode, setDarkMode ,}) {
     localStorage.removeItem("token");
     localStorage.removeItem("username");
 
-    navigate("/login");
-
-    window.location.reload();
+    navigate("/");
 
   };
 
   return (
+    <nav className="navbar">
 
-   <nav className="navbar">
+      <Link to="/" className="logo">
+        BlogSphere
+      </Link>
 
-  <Link to="/" className="logo">
-    BlogSphere
-  </Link>
+      <div className="nav-links">
 
-  <div className="nav-links">
+        {token && (
+          <span className="welcome-text">
+            Welcome, {username}
+          </span>
+        )}
 
-    {
-      token && (
-        <span className="welcome-text">
-          Welcome, {username}
-        </span>
-      )
-    }
-
-    {
-      token && (
-        <Link
-          to="/my-blogs"
-          className="nav-link"
-        >
-          My Blogs
-        </Link>
-      )
-    }
-
-    {
-      token && (
-        <Link
-          to="/create-blog"
-          className="nav-link"
-        >
-          Create Blog
-        </Link>
-      )
-    }
-
-    {
-      token && (
-        <Link
-          to="/profile"
-          className="nav-link"
-        >
-          Profile
-        </Link>
-      )
-    }
-
-    {
-      !token ? (
-        <>
+        {token && (
           <Link
-            to="/login"
+            to="/my-blogs"
             className="nav-link"
           >
-            Login
+            My Blogs
           </Link>
+        )}
 
+        {token && (
           <Link
-            to="/signup"
+            to="/create-blog"
             className="nav-link"
           >
-            Signup
+            Create Blog
           </Link>
-        </>
-      ) : (
-        <button
-          onClick={handleLogout}
-          className="logout-btn"
-        >
-          Logout
-        </button>
-      )
-    }
+        )}
 
-  </div>
+        {token && (
+          <Link
+            to="/profile"
+            className="nav-link"
+          >
+            Profile
+          </Link>
+        )}
 
-</nav>
+        {!token ? (
+          <>
+            <Link
+              to="/login"
+              className="nav-link"
+            >
+              Login
+            </Link>
 
+            <Link
+              to="/signup"
+              className="nav-link"
+            >
+              Signup
+            </Link>
+          </>
+        ) : (
+          <button
+            onClick={handleLogout}
+            className="logout-btn"
+          >
+            Logout
+          </button>
+        )}
+
+      </div>
+
+    </nav>
   );
-
 }
 
 export default Navbar;
